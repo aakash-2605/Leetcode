147359bc1f54c64c97361c2ed26effc0f214c957
Aakash S <116516015+aakash-2605@users.noreply.github.com>
2024-08-21 23:05:08 +0530

    Time: 0 ms (100.00%) | Memory: 56.3 MB (31.87%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
@@ -3,17 +3,6 @@ class Solution {
     public int peakIndexInMountainArray(int[] arr) {
         int start=0;
         int end=arr.length-1;
-        int mid=start+(end - start)/2;
-        if(arr[mid] > arr[mid +1]){
-                end=mid;
-                return binsearch(arr,start,end);
-        }
-        else{
-            start=mid+1;
-            return binsearch(arr,start,end);
-        }   
-        }
-    public int binsearch(int[] arr,int start,int end){
         int mid=0;
         while(start < end) {
             // find the middle element
@@ -21,6 +10,7 @@ class Solution {
             if (arr[mid] > arr[mid+1]) end=mid;
             else start=mid+1;
         }
-        return start;
-    }
+        return start; 
+        }
+  
 }
